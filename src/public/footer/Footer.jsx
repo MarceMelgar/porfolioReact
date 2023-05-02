eafb1a7cb417101528e98bd29ed48c6b73a94372
Marce Melgar <m.melgaba@gmail.com>
2023-05-02 15:30:19 -0400

    margenes footer

--- src/public/footer/Footer.jsx
+++ src/public/footer/Footer.jsx
@@ -4,15 +4,18 @@ import { BsLinkedin, BsGithub, BsTwitter } from 'react-icons/bs'
 const Footer = () => {
   return (
     <footer className='footer-m'>
-        <div className="footer title">
+        <div className="footer-title">
           <h2> Contacto </h2>
         </div>
         <div className="left">
-            <a target="_blank" href="https://github.com/MarceMelgar?tab=repositories" className="link"><BsGithub /><span> Github </span></a>
-            <a href="https://www.linkedin.com/in/marcemelgar/" target='_BLANK'> <BsLinkedin /> Linkedin </a>
+            <a href="https://github.com/MarceMelgar?tab=repositories" target="_blank" rel="noreferrer"><BsGithub /><span> Github </span></a>
+            <a href="https://www.linkedin.com/in/marcemelgar/" target='_blank' rel="noreferrer"> <BsLinkedin /> Linkedin </a>
         </div>
     </footer>
   )
 }
 
 export default Footer
+
+
+/*  className="link" */
